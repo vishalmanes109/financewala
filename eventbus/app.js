50ba12fca523af2139c9757486e322fcebc6ba8e
vishalmanes109 <vishalmanes109@gmail.com>
2021-05-05 19:34:15 +0530

    event driven architecture implementation started

--- eventbus/app.js
+++ eventbus/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 
+const cors = require("cors");
+const eventRouter = require("./eventbus/eventbus.router");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/event", eventRouter);
+
+router.use("/evenbus");
 app = express();
 app.listen(3004, () => {
   console.log("event bus started on port 3004");
--- eventbus/eventbus/eventbus.router.js
+++ eventbus/eventbus/eventbus.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post();
+router.post("/", addMetadata);
 router.post();
 router.get();
 
